ea790bb06fd1a1e424d25909ac40cb1b5bfc9800
iddiaz <diazdiazivan@gmail.com>
2017-03-21 21:40:37 +0100

    Trabajando con ngModel

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
-
+export class TemplateComponent {
+  usuario: Object = {
+    nombre: 'Ivan',
+    apellido: 'Diaz',
+    correo: 'correo@ivandiazdiaz.com'
+  }
   constructor() { }
 
-  ngOnInit() {
+  guardar(forma: NgForm){
+    
+    console.log('ngForm', forma);
+    console.log('valor forma', forma.value);
+    console.log('Usuario', this.usuario);
   }
 
 }

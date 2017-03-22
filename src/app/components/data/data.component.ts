9ef34793d9a78a8722f3ef8dd67e3887c0cb6199
iddiaz <diazdiazivan@gmail.com>
2017-03-22 14:42:41 +0100

    Trabajando con formularios desde la data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
-  }
+  //si el formulario es muy grande es conveniente hacer la construcción en el ngOnInit mejor, para que lo cargue una vez haya cargado la página y no antes.
+  constructor() { 
+    this.forma = new FormGroup({
+      'nombre': new FormControl( '', [ Validators.required,
+                                       Validators.minLength(3)
+        ] ),
+      'apellido': new FormControl( '', Validators.required ),
+      'correo': new FormControl( '', [ Validators.required,
+                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+          ] )
+    });
+   }
+
+   guardarCambios() {
+     console.log(this.forma.value);
+     console.log(this.forma);
+   }
+
 
 }

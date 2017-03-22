d51eda2326f62e1b9fe053d78af60519276c1638
iddiaz <diazdiazivan@gmail.com>
2017-03-22 18:10:11 +0100

    Seterar y resetear datos a estado determinado

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,7 +10,7 @@ export class DataComponent {
 
   forma: FormGroup;
 
-  usuario: Object = {
+  usuario: any = {
     nombrecompleto: {
       nombre: 'Ivan',
       apellido: 'Diaz'
@@ -24,7 +24,8 @@ export class DataComponent {
 
     this.forma = new FormGroup({
       'nombrecompleto': new FormGroup({
-        'nombre': new FormControl( '', [ Validators.required,
+        //1a. -carga de la data al formulario a partir del objeto, este metodo es válido pero nada práctico
+        'nombre': new FormControl( this.usuario.nombrecompleto.nombre, [ Validators.required,
                                          Validators.minLength(3) ] ),
         'apellido': new FormControl( '', Validators.required )
       }),
@@ -32,11 +33,26 @@ export class DataComponent {
                                        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
           ] )
     });
+    //  1b- es más cómodo hacerlo asi, si tiene la misma estructura, recorrera el objeto y le aplicará todos los valores.
+    this.forma.setValue(this.usuario);
    }
+
 
    guardarCambios() {
      console.log(this.forma.value);
      console.log(this.forma);
+
+     //2a.- con reset podemos resetear el formulario a sus valores originales antes de que fuera tocado niongún componente
+     this.forma.reset({
+       nombrecompleto:{
+         nombre: 'Peter',
+         apellido: ''
+       },
+       correo:''
+     });
+
+     //2b.- otro método de hacerlo podria ser así, aunque es más engorroso ya que requiere escribir cada campo.
+     this.forma.controls['correo'].setValue('nuevocorreo@correo.com');
    }
 
 
